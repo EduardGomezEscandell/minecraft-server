53b9e41a225c6b6f2084d7cffd9bad5249d59329
Edu Gómez Escandell <eduard.gomez.escandell@gmail.com>
2024-08-18 19:32:23 +0200

    Small fix in deployment file

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,7 @@
 SSH_TARGET=${SSH_TARGET:-minecraft}
 
 rsync --exclude="bin" --exclude=".git" --recursive ./minecraft-server/ "${SSH_TARGET}:minecraft-server/"
+ssh ${SSH_TARGET} -- "sudo apt update && sudo apt install -y make" # All other dependencies are installed by the makefile
 ssh ${SSH_TARGET} -- "cd minecraft-server && make dependencies && make install && make start"
 ssh ${SSH_TARGET} -- "systemctl status minecraft.service"
+
