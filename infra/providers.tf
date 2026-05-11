041ef0113a9c0653d487b853116a5031e9d6d1fc
Edu Gómez Escandell <eduard.gomez.escandell@gmail.com>
2026-05-11 12:45:27 +0200

    Remote state

--- infra/providers.tf
+++ infra/providers.tf
@@ -5,6 +5,15 @@ terraform {
       version = "~>3.0"
     }
   }
+
+  # You need to create this storage account and container manually before running terraform init
+  # Alternatively, comment out the backend block and keep a local state file.
+  backend "azurerm" {
+      resource_group_name  = "tfstate"
+      storage_account_name = "coleguistfstate"
+      container_name       = "tfstate"
+      key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
